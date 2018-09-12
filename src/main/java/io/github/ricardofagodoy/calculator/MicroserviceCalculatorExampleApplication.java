ba2ed09ef7affed20acf2f1905cc0cc62d056c5d
ricardofagodoy <ricardofagodoy@gmail.com>
2018-09-11 22:24:59 -0300

    spring initializr

---
+++ src/main/java/io/github/ricardofagodoy/calculator/MicroserviceCalculatorExampleApplication.java
@@ -0,0 +1,12 @@
+package io.github.ricardofagodoy.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCalculatorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCalculatorExampleApplication.class, args);
+	}
+}
